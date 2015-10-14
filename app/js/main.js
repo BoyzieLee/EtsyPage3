5065775bb4937283cfccb0b9bbcba0dbbf219776
Boyzie Mathis <boyzielee@gmail.com>
2015-10-13 22:18:55 -0400

    Added footer & more styling.

--- app/js/main.js
+++ app/js/main.js
@@ -2,5 +2,33 @@
 
 (function () {
 
-  console.log('It Works!');
+  var url = 'http://json-data.herokuapp.com/forms';
+
+  var promise = $.getJSON(url);
+
+  promise.then(function (responseFromServer) {
+    doSomething(responseFromServer);
+  });
+
+  var template = _.template($('#registration-form').text());
+  var templateSelect = _.template($('#language-form').text());
+
+  var doSomething = function doSomething(arrayData) {
+
+    _.each(arrayData, function (item) {
+
+      if (item.options.length > 0) {
+        var selectBlock = templateSelect(item);
+        $('.container').append(selectBlock);
+      } else if (item.type === 'text' || item.type === 'tel' || item.type === 'email' || item.type === 'textarea') {
+        var htmlBlock = template(item);
+        $('.container').append(htmlBlock);
+      };
+    });
+  };
+
+  // copy data & store in a variable
+  // including the Header/Sidebar/Links/Search
+  // implement hover events and link the items
+  // to their proper Etsy product pages (items only, other links can be just linked to #)
 })();
--- src/main.js
+++ src/main.js
@@ -1,5 +1,33 @@
 (function () {
 
-  console.log('It Works!');
+  let url = 'http://json-data.herokuapp.com/forms';
 
+  let promise = $.getJSON(url);
+
+  promise.then( function(responseFromServer){
+    doSomething(responseFromServer);
+  });
+
+  let template = _.template($('#registration-form').text());
+  let templateSelect = _.template($('#language-form').text());
+
+  let doSomething = function(arrayData) {
+
+    _.each(arrayData, function(item){
+
+    if (item.options.length > 0) {
+      var selectBlock = templateSelect(item);
+      $('.container').append(selectBlock);     
+    } else if (item.type === 'text' || item.type === 'tel' || item.type === 'email' || item.type === 'textarea') {
+        var htmlBlock = template(item);
+        $('.container').append(htmlBlock);
+      };
+
+    });
+  };
+
+// copy data & store in a variable
+// including the Header/Sidebar/Links/Search
+// implement hover events and link the items
+// to their proper Etsy product pages (items only, other links can be just linked to #)
 }());
